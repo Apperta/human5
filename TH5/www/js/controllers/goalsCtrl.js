8381a4a0aeedee6920821519952c35de0061ca18
Lcc1008 <leyvve1008@gmail.com>
2017-03-03 03:21:10 +0000

    add/delete goals

--- TH5/www/js/controllers/goalsCtrl.js
+++ TH5/www/js/controllers/goalsCtrl.js
@@ -2,6 +2,27 @@ myapp.controller('goalsCtrl', ['$scope', '$stateParams', // The following is the
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+  
+   $scope.todos = [
+     {text:'Hiking', done:false},         
+     {text: 'Afternoon tea', done:false}
+   ];
 
+    $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+   };
+ 	$scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+  
+    // $scope.clearCompleted = function () {
+    //     $scope.todos = _.filter($scope.todos, function(todo){
+    //         return !todo.done;
+    //     });
+    // };
 
+    $scope.removeItem = function(index){
+    $scope.todos.splice(index, 1);
+  }
 }])
--- TH5/www/js/controllers/statisticCtrl.js
+++ TH5/www/js/controllers/statisticCtrl.js
@@ -3,5 +3,54 @@ myapp.controller('statisticCtrl', ['$scope', '$stateParams', // The following is
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  /* Other questions chart */
+    var chartFor = function(){
 
+        $(function () {
+            $('#other-chart').highcharts({
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Other questions summery'
+                },
+                subtitle: {
+                    text: 'Taking from ' + $rootScope.quizCount + ' survey samples'
+                },
+                xAxis: {
+                    categories:$rootScope.labels,
+                    crosshair: true
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Answers'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormatter: function() {
+                        return '<tr><td style="color:'+this.series.color+';padding:0">'+this.series.name+': </td>' +
+                        '<td style="padding:0"><b>'+this.y.toFixed(1)+'</b></td></tr>'+
+                        $rootScope.ansNotes[this.x]
+                    },
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Answer',
+                    data: $rootScope.data
+
+                }]
+            });
+        });
+
+    }; // Chart for function
 }])
