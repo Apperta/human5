9c733d41650333e9589b0e555b362ab75ff8b870
Lcc1008 <leyvve1008@gmail.com>
2017-04-20 02:11:41 +0100

    post for sync

--- TH5/www/js/controllers/regCtrl.js
+++ TH5/www/js/controllers/regCtrl.js
@@ -42,14 +42,14 @@ myapp.controller('regCtrl', ['$scope', '$state', '$stateParams','$localStorage',
     if($localStorage.userId != 0)
     {
       $.post("http://51.140.39.138:3000/users/verify", {s_id:$localStorage.userId}, 
-          function( data ) 
+        function( data ) 
+        {
+          if(data.s_id != -1)
           {
-              if(data.s_id != -1)
-            {
-                $localStorage.userId = data.s_id;
-                $state.go('menu.tH5');
-            }
-          },"json"); 
+            $localStorage.userId = data.s_id;
+            $state.go('menu.tH5');
+          }
+        },"json"); 
     }
 
 
@@ -68,75 +68,119 @@ myapp.controller('regCtrl', ['$scope', '$state', '$stateParams','$localStorage',
       {
         $scope.badLogin = true;
           //check if there is login information on user's device
-              if ($localStorage.localLogin[0].email == $scope.username) {
-                if ($localStorage.localLogin[0].password == $scope.password) {
-                  console.log("heyy");
-                  $scope.username = "";
-                  $scope.password = "";
-                  $state.go('menu.tH5');
-                } else {
-                  $scope.badLogin = false;
-                  $scope.password = "";
-                }
-              }
-            console.log($localStorage.localLogin[0]);
-
-
-        $.post("http://51.140.39.138:3000/users/login", { email: $scope.username, password:$scope.password}, 
-          function( data ) 
-          {
-            console.log(data.s_id);
-            if (data.s_id != -1)
-            {
-              $localStorage.userId = data.s_id;
+          if ($localStorage.localLogin[0].email == $scope.username) {
+            if ($localStorage.localLogin[0].password == $scope.password) {
+              console.log("heyy");
+              $scope.username = "";
+              $scope.password = "";
               $state.go('menu.tH5');
-            }
-            if (data.s_id == -1)
-            {
-              $localStorage.userId = data.s_id;
+            } else {
               $scope.badLogin = false;
               $scope.password = "";
             }
-          },"json");  
-      }
-    }
+          }
+          console.log($localStorage.localLogin[0]);
 
-    $scope.registerSubmit = function()
-    {
+
+          $.post("http://51.140.39.138:3000/users/login", { email: $scope.username, password:$scope.password}, 
+            function( data ) 
+            {
+              console.log(data.s_id);
+              if (data.s_id != -1)
+              {
+                $localStorage.userId = data.s_id;
+                $state.go('menu.tH5');
+              }
+              if (data.s_id == -1)
+              {
+                $localStorage.userId = data.s_id;
+                $scope.badLogin = false;
+                $scope.password = "";
+              }
+            },"json");  
+        }
+      }
+
+      $scope.registerSubmit = function()
+      {
       //local register
       
         // $localStorage.localLogin.push({
         //   email:$scope.username,
         //   password:$scope.password
         // });
-      $localStorage.localLogin[0].email = $scope.username;
-      $localStorage.localLogin[0].password = $scope.password;
+        $localStorage.localLogin[0].email = $scope.username;
+        $localStorage.localLogin[0].password = $scope.password;
 
-    console.log($localStorage.localLogin[0]);
+        console.log($localStorage.localLogin[0]);
 
-      $.post("http://51.140.39.138:3000/users/register", { name:$scope.name, email: $scope.username, password:$scope.password}, 
-        function( data ) 
-        {
-          console.log(data.s_id);
-          if (data.s_id != -1)
+        $.post("http://51.140.39.138:3000/users/register", { name:$scope.name, email: $scope.username, password:$scope.password}, 
+          function( data ) 
           {
-            $state.go('login');
-          }
-        },"json");
-      
-    
-    }
+            console.log(data.s_id);
+            if (data.s_id != -1)
+            {
+              $state.go('login');
+            }
+          },"json");
 
 
-    /* Facebook login */
-    $rootScope.$on('event:social-sign-in-success', function(event, userDetails)
-    {
-      console.log(userDetails);
-      $localStorage.userDetails = userDetails;
-
-      $state.go('menu.tH5');
-    });
+      }
 
 
+      /* Facebook login */
+      $rootScope.$on('event:social-sign-in-success', function(event, userDetails)
+      {
+        console.log(userDetails);
+        $localStorage.userDetails = userDetails;
 
-  }])
+        $state.go('menu.tH5');
+      });
+
+
+
+      $scope.sync = function(){
+        $.post("http://51.140.39.138:3000/users/verify", {email:$localStorage.localLogin[0].email}, 
+          function( data ) 
+          {
+            if(data.email != "")
+            {
+              
+            }
+          },"json"); 
+
+
+
+
+        //post data onto server after request for the User id
+        for (var i = $localStorage.selfRateList.length - 1; i >= 0; i--) {
+          $.post("http://51.140.39.138:3000/sync/th5", {mind:$localStorage.selfRateList[i].mind, movement:$localStorage.selfRateList[i].movement, nutrition:$localStorage.selfRateList[i].nutrition, world:$localStorage.selfRateList[i].world, body:$localStorage.selfRateList[i].body}, 
+            function( data ) 
+            {
+
+            },"json");
+        } 
+        for (var i = $localStorage.YDC.length - 1; i >= 0; i--) {
+          $.post("http://51.140.39.138:3000/sync/stress", {val:$localStorage.YDC[i]}, 
+            function( data ) 
+            {
+
+            },"json");
+        } 
+        for (var i = $localStorage.WEMWBS.length - 1; i >= 0; i--) {
+          $.post("http://51.140.39.138:3000/sync/wemb", {val1:$localStorage.WEMWBS[i].val1,val2:$localStorage.WEMWBS[i].val2,val3:$localStorage.WEMWBS[i].val3,val4:$localStorage.WEMWBS[i].val4,val5:$localStorage.WEMWBS[i].val5,val6:$localStorage.WEMWBS[i].val6,val7:$localStorage.WEMWBS[i].val7}, 
+            function( data ) 
+            {
+
+            },"json");
+        } 
+        for (var i = $localStorage.todolist.length - 1; i >= 0; i--) {
+          $.post("http://51.140.39.138:3000/sync/goals", {text:$localStorage.todolist[i].text, done:$localStorage.todolist[i].done, type:$localStorage.todolist[i].type, deadline:$localStorage.todolist[i].deadline}, 
+            function( data ) 
+            {
+
+            },"json");
+        } 
+      }
+
+    }])
